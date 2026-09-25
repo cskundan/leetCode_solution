039c91438be6f5ef86ff535fad1c423057b52606
KUNDAN KUMAR <cskundan32@gmail.com>
2026-09-26 02:32:41 +0530

    Time: 11 ms (82.79%), Space: 21.9 MB (79.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,10 +3,14 @@ class Solution:
         dict1 = {}
         for s in strs:
             key = "".join(sorted(s))
+            print(key)
             if key in dict1:
                 dict1[key].append(s)
             else:
                 dict1[key] = [s]
+
+
+                
         return list(dict1.values())
 
         
